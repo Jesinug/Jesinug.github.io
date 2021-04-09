6254c3e2a35ba5802207023b2dd1c67d5d2d413f
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-09 23:25:31 +0200

    new version with one plant

--- src/game.js
+++ src/game.js
@@ -26,7 +26,6 @@ class Game {
     this.canvas.setAttribute("height", this.containerHeight);
 
     this.player = new Player(this.canvas, 5, "images/plant_state_0.png");
-    console.log(this.player)
 
     function handleKeyDown(event) {
       if (event.keyCode === 39) {
@@ -74,11 +73,6 @@ class Game {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
       this.player.draw();
-      /*if(this.lives >= 10) {
-        this.player.draw(this.lives) 
-      } else {
-        this.player.draw()
-      }*/
 
       this.drops.forEach((enemy) => {
         enemy.draw();
